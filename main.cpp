93442eb764dfdb3ad0839af0f6e877a7bc566a94
Jader <jvjader@gmail.com>
2021-09-16 13:36:44 +0200

    added comment

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/* git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit" */
+
 int main()
 {
-   printf("Hello, World!");
+   printf("hella lennart!");
    return 0;
 }
